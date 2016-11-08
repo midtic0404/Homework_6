a9bc079a8509875f886a28caf73b05b89f1d34f4
Chia Che Chang <midtic0404@gmail.com>
2016-11-08 22:50:15 +0000

    Adding error_dict to test

--- Chia_Chang_Hw2.py
+++ Chia_Chang_Hw2.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 import sys
 import re
+import heapq
 from urllib.request import urlopen
 
 def process():
     with urlopen("http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.test") as log:
-        test = set()
+        error_dict = dict()
+        wordsArray = [] #big list storing each line as list
         for line in log:
-            words = line.decode('utf-8').split(" ")
-            test.add(words[7])
+            words = line.decode('utf-8')
+            words = words.replace("\n"," ")
+            wordsList = words.split(" ") #a list as a line separate by " "
+            wordsArray.append(wordsList) #adding small list to big list
+            
+            if wordsList[12] in error_dict:
+                error_dict[wordsList[12]] += 1
 
-        print(test)
+            if not wordsList[12] in error_dict:
+                error_dict[wordsList[12]] = 0
+                error_dict[wordsList[12]] += 1
+        print(len(error_dict)) #test to see how many keys in dict 
         
 def main():
     process()
