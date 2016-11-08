4fc0404f3741369ca0d04d0dd85be20b04e07474
Chia Che Chang <midtic0404@gmail.com>
2016-11-08 03:42:57 +0000

    Add urlopen to read the data from the link

--- Chia_Chang_Hw2.py
+++ Chia_Chang_Hw2.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 import sys
+import re
+from urllib.request import urlopen
 
+def process():
+    with urlopen("http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.test") as log:
+        for line in log:
+            words = line.decode('utf-8').split()
+            print(words)
 
 
 def main():
-    pass
+    process()
 
 
 if __name__ == "__main__":
