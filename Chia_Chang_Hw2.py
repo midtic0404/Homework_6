bb68d75e1c1cec89fba5989d9875e8a7f717b872
Chia Che Chang <midtic0404@gmail.com>
2016-11-08 18:54:51 +0000

    Modified process() function

--- Chia_Chang_Hw2.py
+++ Chia_Chang_Hw2.py
@@ -6,9 +6,8 @@ from urllib.request import urlopen
 def process():
     with urlopen("http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.test") as log:
         for line in log:
-            words = line.decode('utf-8').split()
+            words = line.decode('utf-8').split(" ")
             print(words)
-
 
 def main():
     process()
