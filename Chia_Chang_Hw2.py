0cc7e86efe0ae85db8ab9f6c30fb804bf709e503
Chia Che Chang <midtic0404@gmail.com>
2016-11-09 04:56:41 +0000

    Adding logic to add error to dict

--- Chia_Chang_Hw2.py
+++ Chia_Chang_Hw2.py
@@ -5,22 +5,30 @@ import heapq
 from urllib.request import urlopen
 
 def process():
-    with urlopen("http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.test") as log:
+    with urlopen("http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.full") as log:
         error_dict = dict()
         wordsArray = [] #big list storing each line as list
         for line in log:
             words = line.decode('utf-8')
-            words = words.replace("\n"," ")
-            wordsList = words.split(" ") #a list as a line separate by " "
-            wordsArray.append(wordsList) #adding small list to big list
+            parts = words.partition('client')
+            checkparts = parts[2] #string to be check
+        
+           # pat = re.compile('/[\w+/.-]+')#test from that site
+            match = re.search('/[\w+/._~-]+',parts[2])
+            if match:
+                errorpath = match.group()
+                
+                if errorpath not in error_dict:
+                    error_dict[errorpath] = 1
+                else:
+                    error_dict[errorpath] += 1
             
-            if wordsList[12] in error_dict:
-                error_dict[wordsList[12]] += 1
 
-            if not wordsList[12] in error_dict:
-                error_dict[wordsList[12]] = 0
-                error_dict[wordsList[12]] += 1
-        print(len(error_dict)) #test to see how many keys in dict 
+        if "/var/www/html/_c21_" in error_dict:
+            print("Yes" , error_dict["/var/www/html/_c21_"])
+        else:
+            print("no")
+
         
 def main():
     process()
